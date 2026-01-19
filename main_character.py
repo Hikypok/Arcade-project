b6272d634e1a43b98f82b61d3125bbb9b77b6a79
Hikypok <renatovnaalina@gmail.com>
2026-01-19 03:00:18 +0300

    Create main_character.py

---
+++ main_character.py
@@ -0,0 +1,46 @@
+import arcade
+
+
+class SheriffBorya(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.speed = 5
+        self.idle_texture = arcade.load_texture("images/sprite.png")
+        self.walk_right = arcade.load_texture("images/sprite_right.png")
+        self.walk_left = arcade.load_texture("images/sprite_left.png")
+        self.walk_up = arcade.load_texture("images/sprite_up.png")
+        self.walk_down = arcade.load_texture("images/sprite_down.png")
+
+        self.texture = self.idle_texture
+        self.center_x = 300
+        self.center_y = 200
+        self.change_x = 0
+        self.change_y = 0
+
+    def update_animation(self, delta_time: float = 1/60):
+        if self.change_x > 0:
+            self.texture = self.walk_right
+        elif self.change_x < 0:
+            self.texture = self.walk_left
+        elif self.change_y > 0:
+            self.texture = self.walk_up
+        elif self.change_y < 0:
+            self.texture = self.walk_down
+        else:
+            self.texture = self.idle_texture
+
+    def move(self, left: bool, right: bool, up: bool, down: bool):
+        self.change_x = 0
+        self.change_y = 0
+
+        if left:
+            self.change_x = -self.speed
+        if right:
+            self.change_x = self.speed
+        if up:
+            self.change_y = self.speed
+        if down:
+            self.change_y = -self.speed
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
