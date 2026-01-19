bda7bceaa7f653d373e3851b7bb28f07dd6ef459
Hikypok <renatovnaalina@gmail.com>
2026-01-19 03:04:34 +0300

    Create dialog_system.py

---
+++ dialog_system.py
@@ -0,0 +1,80 @@
+import arcade
+
+
+class DialogueBox:
+    def __init__(self, window_width, window_height):
+        self.window_width = window_width
+        self.window_height = window_height
+        self.visible = False
+        self.lines = []
+        self.speaker = ""
+        self.on_close = None
+
+    def show(self, speaker, text, on_close=None):
+        self.speaker = speaker
+        self.lines = self._split_text(text, max_width=self.window_width - 140)
+        self.visible = True
+        self.on_close = on_close
+
+    def _split_text(self, text, max_width):
+        words = text.split()
+        lines = []
+        current_line = ""
+        for word in words:
+            test_line = current_line + " " + word if current_line else word
+            if len(test_line) * 8 > max_width:
+                if current_line:
+                    lines.append(current_line)
+                current_line = word
+            else:
+                current_line = test_line
+        if current_line:
+            lines.append(current_line)
+        return lines
+
+    def draw(self):
+        if not self.visible:
+            return
+
+        width = self.window_width - 2 * 50
+        height = 150
+
+        arcade.draw_rect_filled(
+            arcade.rect.XYWH(500, 50, width, height),
+            color=arcade.color.GRAY_ASPARAGUS)
+
+        arcade.draw_rect_outline(
+            arcade.rect.XYWH(500, 50, width, height),
+            color=arcade.color.WHITE,
+            border_width=3)
+
+        if self.speaker:
+            arcade.draw_text(
+                f"{self.speaker}:",
+                120,
+                height - 50,
+                arcade.color.YELLOW,
+                16,
+                bold=True)
+
+        text_y = height - 70
+        for line in self.lines:
+            arcade.draw_text(
+                line,
+                100,
+                text_y,
+                arcade.color.WHITE,
+                14)
+            text_y -= 20
+
+        arcade.draw_text(
+            "[Нажмите SPACE, чтобы продолжить]",
+            350,
+            20,
+            arcade.color.ASH_GREY,
+            12)
+
+    def close(self):
+        self.visible = False
+        if self.on_close:
+            self.on_close()
